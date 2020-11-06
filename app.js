59c93d62ef157db37218feb376c52b74bf758e6c
Alexander Garcia <alexg97343@gmail.com>
2020-11-06 13:48:22 -0500

    placed an if statement so if a game is already running then the user may not restart one

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
-const getPlayerChoice = function() {
-  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
-  if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ) {
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
     return DEFAULT_USER_CHOICE;
   }
   return selection;
 };
 
-startGameBtn.addEventListener('click', function() {
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
   console.log(playerSelection);
